99d88198f21f1bb33bff8daf6b4c1b405946c3a2
bavtim <40119744+bavtim@users.noreply.github.com>
2022-03-21 21:45:06 +1000

    press f to press f
    press f to press f

--- Main.gd
+++ Main.gd
@@ -4,6 +4,7 @@ export (PackedScene) var mob_scene
 
 func _ready():
 	randomize()
+	$Retry.hide()
 
 func _on_MobTimer_timeout():
 	var mob = mob_scene.instance()
@@ -16,7 +17,10 @@ func _on_MobTimer_timeout():
 	mob.initialize(mob_spawn_location.translation,player_position)
 	mob.connect("squashed",$UserInterface/ScoreLabel,"_on_Mob_squashed")
 
-
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_accept") and $Retry.visible:
+		get_tree().reload_current_scene()
 
 func _on_Player_hit():
 	$MobTimer.stop()
+	$Retry.show()
